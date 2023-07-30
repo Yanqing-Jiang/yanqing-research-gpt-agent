c3194eee1935d59df40569a36226bb377283f26f
Yanqing Jiang <94762357+Yanqing-Jiang@users.noreply.github.com>
2023-07-30 18:23:14 -0500

    Update Yanqing_Online_GPT_Agent.py

--- Yanqing_Online_GPT_Agent.py
+++ Yanqing_Online_GPT_Agent.py
@@ -188,7 +188,7 @@ def detect_language(text):
 def main():
     st.set_page_config(page_title="AI research agent", page_icon=":bird:")
 
-    st.header("Yanqing's online research agent 🔮")
+    st.header("Yanqing's online GPT agent 🔮")
     query = st.text_input("Research question")
 
     # Add "Try" text in the first column
